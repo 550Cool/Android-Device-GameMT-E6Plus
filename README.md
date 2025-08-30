d01a26586f1a1518a80873179a425de69ca336ea
550Cool <1665878969@qq.com>
2025-08-30 17:53:04 +0800

    Update File

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for hhhc yx15 (rk3566_hgo)
+
+```
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ device.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/hhhc/rk3566_hgo
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
